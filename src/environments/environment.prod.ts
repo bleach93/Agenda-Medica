392eabe57c390a91085fd28033c80764a8202e23
2018030489 <2018030489@upsin.edu.mx>
2022-01-24 22:43:29 -0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyB4VHEAmfzITt-vs8w7LOe6WJnM0io7Skc",
+			authDomain: "agenda-medica-4ec06.firebaseapp.com",
+			projectId: "agenda-medica-4ec06",
+			storageBucket: "agenda-medica-4ec06.appspot.com",
+			messagingSenderId: "91100925445",
+			appId: "1:91100925445:web:60233f6e4042906bdd566c"
+};
